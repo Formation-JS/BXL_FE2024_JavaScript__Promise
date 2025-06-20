6296964bcedbcb8cc8da178ab9617cb8806eeb91
Aurélien St <aurelien.strimelle@bstorm.be>
2025-06-20 08:50:13 +0200

    Refactoring 🍔

--- js/script2.js
+++ js/script2.js
@@ -1,18 +1,32 @@
+//! Les méthodes utils
 function delay(time) {
     return new Promise((resolve) => {
         setTimeout(resolve, time);
     });
 }
+
+function randomNumberGenerator(min, max, decimal = 2) {
+    const precision = (10 ** decimal);
+    const range = (max - min) * precision;
+    const offset = min * precision;
+
+    return Math.round((Math.random() * range) + offset) / precision
+}
+
+
+//! Les méthodes asynchrone
 async function prepareCoffee() {
     console.log(`Le café est en cours de préparation`);
     await delay(25_000);
     console.log(`Le café est prêt !`);
 }
+
 async function servirCoffee() {
     console.log(`Le café est en cours de service`);
     await delay(5_000);
     console.log(`Le café est servi !`);
 }
+
 async function prepareSalade(...legumes) {
     console.log('Préparation salade');
     for (const legume of legumes) {
@@ -22,27 +36,18 @@ async function prepareSalade(...legumes) {
     await delay(5_000);
     console.log('La salade est prête');
 }
+
 async function ajouterLaVinegrette() {
     console.log('Ajout de la vinegrette ...');
     await delay(5_000)
     console.log('La vinegrette  a été ajoutée !!!');
-
 }
 
 async function chaufferLaPoele() {
     console.log("On commence à chauffer la poele !")
-    const time = randomTimer(15, 20);
+    const time = randomNumberGenerator(15, 20);
     await delay(time * 1000)
     console.log("La poele est chaude !");
-}
-
-function randomTimer(min, max, decimal = 2) {
-    const precision = (10 ** decimal);
-    const range = (max - min) * precision;
-    const offset = min * precision;
-    const time = Math.round((Math.random() * range) + offset) / precision
-
-    return time;
 }
 
 async function servirLaViande() {
@@ -53,19 +58,22 @@ async function servirLaViande() {
 
 async function faireCuireLaViande() {
     /// faire cuire la viande
-    console.log("on cuit la viande");
+    console.log("On cuit la viande");
     await delay(15000);
 
-    if (randomTimer(0, 4, 0) === 1) {
-        throw new Error("la viande est cramé");
+    if (randomNumberGenerator(0, 4, 0) === 1) {
+        throw new Error("La viande est cramé.");
     }
     /// si ça se passe bien affiche réussi
-    console.log('la viande est bien cuit');
+    console.log('La viande est bien cuit.');
 }
 
-//? La fonction qui prépare le repas
 
-async function preparerLeRepas() {
+//! Utilisation des méthodes async
+//? - Enchainnement des méthodes avec "await"
+// Non optimisé car chaque tache se lance les une après les autres...
+async function preparerLeRepas_v1() {
+    console.log("Debut de la préparation du repas");
     try {
         await prepareCoffee();
         await servirCoffee();
@@ -74,11 +82,10 @@ async function preparerLeRepas() {
         await chaufferLaPoele();
         await faireCuireLaViande();
         await servirLaViande();
-        console.log('Le repas est prêt');
+        console.log('Le repas est prêt !');
     } catch (error) {
-        console.log('Désolé je vous appelle au resto !');
         console.error(error)
+        console.log('Le repas est foiré !');
     }
-
 }
-preparerLeRepas() ;
+preparerLeRepas_v1() ;
